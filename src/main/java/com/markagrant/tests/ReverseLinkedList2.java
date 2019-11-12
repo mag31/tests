0518b66e5c88249f27a14d19b1a8b705d0f7b53a
Mark Grant <mark@markagrant.com>
2019-11-12 21:41:45 +0000

    Created custom LinkedList (mutable) and simple test cases

--- src/main/java/com/markagrant/tests/ReverseLinkedList2.java
+++ src/main/java/com/markagrant/tests/ReverseLinkedList2.java
@@ -1,9 +1,50 @@
 package com.markagrant.tests;
 
-import java.util.LinkedList;
-
 public class ReverseLinkedList2 {
-    public LinkedList<Integer> reverseList(LinkedList<Integer> integers, int m, int n) {
-        return integers;
+
+    static class Node {
+
+        int value;
+
+        Node next;
+
+        Node(int value, Node next) {
+           this.value = value;
+           this.next = next;
+        }
+
+        Node repointNext(Node node) {
+            Node oldNext = next;
+            next = node;
+            return oldNext;
+        }
+
+        boolean isTerminalNode() {
+            return next == null;
+        }
+    }
+
+    static Node empty = new Node(-1, null);
+
+    static Node list(int ... values) {
+        Node node = empty;
+        for(int i = values.length - 1; i >= 0; i--) {
+            node = new Node(values[i], node);
+        }
+
+        return node;
+    }
+
+    Node reverseList(Node first, int m, int n) {
+        if(first.isTerminalNode())
+            return first;
+
+        Node second = first.repointNext(empty);
+        Node third = second.repointNext(first);
+        Node forth = third.repointNext(second);
+        Node fifth = forth.repointNext(third);
+        fifth.repointNext(forth);
+
+        return fifth;
     }
 }
--- src/test/java/com/markagrant/tests/ReverseLinkedList2Test.java
+++ src/test/java/com/markagrant/tests/ReverseLinkedList2Test.java
@@ -1,24 +1,35 @@
 package com.markagrant.tests;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.LinkedList;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ReverseLinkedList2Test {
 
-    @Test
-    public void can_create_class(){
-        new ReverseLinkedList2();
+    private ReverseLinkedList2 reverse;
+
+    @Before
+    public void setUp() {
+        reverse = new ReverseLinkedList2();
     }
 
     @Test
     public void can_reverse_empty_list() {
-        ReverseLinkedList2 reverse = new ReverseLinkedList2();
+        assertTrue(reverse.reverseList(ReverseLinkedList2.empty, 0, 0).isTerminalNode());
+    }
 
-        LinkedList<Integer> results = reverse.reverseList(new LinkedList<>(), 0, 0);
+    @Test
+    public void reverse_whole_list() {
+        ReverseLinkedList2.Node list = ReverseLinkedList2.list(1, 2, 3, 4, 5);
 
-        assertEquals(0, results.size());
+        ReverseLinkedList2.Node  results = reverse.reverseList(list, 0, 5);
+
+        assertEquals(5, results.value);
+        assertEquals(4, results.next.value);
+        assertEquals(3, results.next.next.value);
+        assertEquals(2, results.next.next.next.value);
+        assertEquals(1, results.next.next.next.next.value);
     }
 }

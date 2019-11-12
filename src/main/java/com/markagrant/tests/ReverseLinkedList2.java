62be870898f7fa2fa3a7e4e67ca9eedb73de6e30
Mark Grant <mark@markagrant.com>
2019-11-12 20:52:15 +0000

    Passing simple test to create function api

--- src/main/java/com/markagrant/tests/ReverseLinkedList2.java
+++ src/main/java/com/markagrant/tests/ReverseLinkedList2.java
@@ -1,4 +1,9 @@
 package com.markagrant.tests;
 
+import java.util.LinkedList;
+
 public class ReverseLinkedList2 {
+    public LinkedList<Integer> reverseList(LinkedList<Integer> integers, int m, int n) {
+        return integers;
+    }
 }
--- src/test/java/com/markagrant/tests/ReverseLinkedList2Test.java
+++ src/test/java/com/markagrant/tests/ReverseLinkedList2Test.java
@@ -2,10 +2,23 @@ package com.markagrant.tests;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
 public class ReverseLinkedList2Test {
 
     @Test
     public void can_create_class(){
         new ReverseLinkedList2();
+    }
+
+    @Test
+    public void can_reverse_empty_list() {
+        ReverseLinkedList2 reverse = new ReverseLinkedList2();
+
+        LinkedList<Integer> results = reverse.reverseList(new LinkedList<>(), 0, 0);
+
+        assertEquals(0, results.size());
     }
 }

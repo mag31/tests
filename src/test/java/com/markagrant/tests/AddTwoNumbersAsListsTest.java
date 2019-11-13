7be572a7ea0d924dcf84f29dc5e6511fb6c210e4
Mark Grant <mark@markagrant.com>
2019-11-13 21:05:55 +0000

    Starting Add Two Numbers As Lists puzzle

---
+++ src/test/java/com/markagrant/tests/AddTwoNumbersAsListsTest.java
@@ -0,0 +1,11 @@
+package com.markagrant.tests;
+
+import org.junit.Test;
+
+public class AddTwoNumbersAsListsTest {
+
+    @Test
+    public void can_create_class() {
+        new AddTwoNumbersAsLists();
+    }
+}

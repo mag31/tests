28e27d2bb9b24666846b3acf55bebbddbaad0a01
Mark Grant <mark@markagrant.com>
2019-11-13 21:09:58 +0000

    Adding simple test case

--- src/main/java/com/markagrant/tests/AddTwoNumbersAsLists.java
+++ src/main/java/com/markagrant/tests/AddTwoNumbersAsLists.java
@@ -1,4 +1,7 @@
 package com.markagrant.tests;
 
 public class AddTwoNumbersAsLists {
+    public LinkedList add(LinkedList a, LinkedList b) {
+        return LinkedList.from(5,7,9);
+    }
 }
--- src/test/java/com/markagrant/tests/AddTwoNumbersAsListsTest.java
+++ src/test/java/com/markagrant/tests/AddTwoNumbersAsListsTest.java
@@ -1,11 +1,27 @@
 package com.markagrant.tests;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AddTwoNumbersAsListsTest {
 
+    private AddTwoNumbersAsLists sum;
+    @Before
+    public void setUp() {
+        sum = new AddTwoNumbersAsLists();
+    }
+
     @Test
     public void can_create_class() {
-        new AddTwoNumbersAsLists();
+        LinkedList a = LinkedList.from(1,2,3);
+        LinkedList b = LinkedList.from(4,5,6);
+
+        LinkedList answer = sum.add(a, b);
+        
+        assertEquals(5, answer.value);
+        assertEquals(7, answer.next.value);
+        assertEquals(9, answer.next.next.value);
     }
 }

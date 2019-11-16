b0767c212a78752f90e8725d956354fe15b3093e
Mark Grant <mark@markagrant.com>
2019-11-16 22:33:06 +0000

    All test examples pass

--- src/main/java/com/markagrant/tests/RegularExpression2.java
+++ src/main/java/com/markagrant/tests/RegularExpression2.java
@@ -2,6 +2,44 @@ package com.markagrant.tests;
 
 public class RegularExpression2 {
     public boolean isMatch(String text, String pattern) {
+        int j = 0;
+
+        char preceeding = '$';
+        for(int i = 0; i < text.length(); i++) {
+            char t = text.charAt(i);
+
+            if(j == pattern.length())
+                return false;
+
+            char p = pattern.charAt(j);
+
+            if(p == '.') {
+                j++;
+                preceeding = '.';
+                continue;
+            }
+
+            if(p == '*') {
+                if(t == preceeding)
+                    continue;
+                if(preceeding == '.' )
+                    continue;
+                // zero length match
+                if(j + 1 < pattern.length() && t == pattern.charAt(++j)) {
+                    preceeding = t;
+                    j++;
+                    continue;
+                }
+
+                return false;
+            }
+
+            if(p != t)
+                return false;
+
+            preceeding = t;
+            j++;
+        }
         return true;
     }
 }
--- src/test/java/com/markagrant/tests/RegularExpression2Test.java
+++ src/test/java/com/markagrant/tests/RegularExpression2Test.java
@@ -2,6 +2,7 @@ package com.markagrant.tests;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class RegularExpression2Test {
@@ -10,5 +11,47 @@ public class RegularExpression2Test {
     public void can_match_simple_text() {
         RegularExpression2 reg = new RegularExpression2();
         assertTrue(reg.isMatch("a", "a"));
+    }
+
+    @Test
+    public void can_match_with_no_entire_string() {
+        RegularExpression2 reg = new RegularExpression2();
+        assertFalse(reg.isMatch("aa", "a"));
+    }
+
+    @Test
+    public void aa_matches_aa() {
+        RegularExpression2 reg = new RegularExpression2();
+        assertTrue(reg.isMatch("aa", "aa"));
+    }
+
+    @Test
+    public void aa__no_match_aaa() {
+        RegularExpression2 reg = new RegularExpression2();
+        assertFalse(reg.isMatch("aaa", "aa"));
+    }
+
+    @Test
+    public void a_star__matches_aa() {
+        RegularExpression2 reg = new RegularExpression2();
+        assertTrue(reg.isMatch("aa", "a*"));
+    }
+
+    @Test
+    public void dot_star__matches_aa() {
+        RegularExpression2 reg = new RegularExpression2();
+        assertTrue(reg.isMatch("aa", ".*"));
+    }
+
+    @Test
+    public void dot_star__matches_ab() {
+        RegularExpression2 reg = new RegularExpression2();
+        assertTrue(reg.isMatch("ab", ".*"));
+    }
+
+    @Test
+    public void c_star_a_star_b__matches_cab() {
+        RegularExpression2 reg = new RegularExpression2();
+        assertTrue(reg.isMatch("cab", "c*a*b"));
     }
 }
